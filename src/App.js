d5edd37673cbf250cc80e294dd5555978e5c34dc
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-10-08 21:20:50 +0530

    Loading With Data Fetch From API Integrated

--- src/App.js
+++ src/App.js
@@ -1,77 +1,32 @@
+import { useState, useEffect } from "react";
+import loader from "./loader.jpg";
+
 import "./App.css";
 
-const questionAnswerSet = [
-  {
-    question: "What is the capital of the United States?",
-    answerOptions: ["Washington", "New York", "Los Angeles", "Chicago"],
-    correctAnswer: "Washington",
-  },
-  {
-    question: "What is the capital of Canada?",
-    answerOptions: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
-    correctAnswer: "Ottawa",
-  },
-  {
-    question: "What is the capital of France?",
-    answerOptions: ["Paris", "Lyon", "Marseille", "Toulouse"],
-    correctAnswer: "Paris",
-  },
-  {
-    question: "What is the capital of Germany?",
-    answerOptions: ["Berlin", "Munich", "Hamburg", "Frankfurt"],
-    correctAnswer: "Berlin",
-  },
-  {
-    question: "What is the capital of Italy?",
-    answerOptions: ["Rome", "Milan", "Naples", "Turin"],
-    correctAnswer: "Rome",
-  },
-  {
-    question: "What is the capital of Spain?",
-    answerOptions: ["Madrid", "Barcelona", "Valencia", "Seville"],
-    correctAnswer: "Madrid",
-  },
-  {
-    question: "What is the capital of Japan?",
-    answerOptions: ["Tokyo", "Kyoto", "Osaka", "Nagoya"],
-    correctAnswer: "Tokyo",
-  },
-  {
-    question: "What is the capital of China?",
-    answerOptions: ["Beijing", "Shanghai", "Hong Kong", "Guangzhou"],
-    correctAnswer: "Beijing",
-  },
-  {
-    question: "What is the capital of Australia?",
-    answerOptions: ["Sydney", "Melbourne", "Perth", "Canberra"],
-    correctAnswer: "Canberra",
-  },
-  {
-    question: "What is the capital of Brazil?",
-    answerOptions: ["Rio de Janeiro", "Sao Paulo", "Salvador", "Brasilia"],
-    correctAnswer: "Brasilia",
-  },
-];
-
-function MainArea() {
+function MainArea({ questionAnswerSet }) {
   return (
     <div className="main-area">
       {questionAnswerSet.map((element) => {
         return (
-          <div className="card-container" key={questionAnswerSet.indexOf(element)}>
+          <div
+            className="card-container"
+            key={questionAnswerSet.indexOf(element)}
+          >
             <div className="front-view">
               <p>
                 <b>{element.question}</b>
               </p>
               <ul>
                 {element.answerOptions.map((option) => {
-                  return <li key={element.answerOptions.indexOf(option)}>{option}</li>;
+                  return (
+                    <li key={element.answerOptions.indexOf(option)}>
+                      {element.answerOptions.indexOf(option) + 1}. {option}
+                    </li>
+                  );
                 })}
               </ul>
             </div>
-            <div className="back-view">
-           {element.correctAnswer}
-            </div>
+            <div className="back-view">{element.correctAnswer}</div>
           </div>
         );
       })}
@@ -80,10 +35,43 @@ function MainArea() {
 }
 
 function StartWork() {
+  const [questionAnswerSet, setQuestionAnswerSet] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=50&type=multiple")
+      .then((response) => response.json())
+      .then((data) => {
+        if (questionAnswerSet.length === 0) {
+          console.log(data);
+          let tempContainer = [];
+          data.results.forEach((element) => {
+            let tempAnswerOptions = element.incorrect_answers.concat(
+              element.correct_answer
+            );
+
+            tempContainer.push({
+              question: element.question,
+              answerOptions: tempAnswerOptions.sort(() => Math.random() - 0.4),
+              correctAnswer: element.correct_answer,
+            });
+          });
+
+          setQuestionAnswerSet(tempContainer);
+        }
+      });
+  }, [questionAnswerSet]);
+
+  if (questionAnswerSet.length === 0)
+    return (
+      <div className="start-work">
+        <img src={loader} alt="loader" />
+      </div>
+    );
+
   return (
     <div className="container">
       <h1>General Knowledge</h1>
-      <MainArea />
+      <MainArea questionAnswerSet={questionAnswerSet} />
     </div>
   );
 }
